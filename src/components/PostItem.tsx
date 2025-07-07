3990bd5ec105a612bbde85c991805058a84a86f2
Patricio M. <patricio1984@gmail.com>
2025-07-07 11:46:42 -0300

    a11y: fixing focus issue in the post item links

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -9,26 +9,28 @@ export const PostItem: React.FC<{ post: Post }> = ({ post }) => {
   const router = useRouter();
 
   return (
-    <li
-      onClick={() => router.push(`/posts/${post.id}`)}
-      className="cursor-pointer list-none"
-    >
-      <FeedItemLayout
-        avatarUrl={`https://i.pravatar.cc/150?u=${post.userId}`}
-        header={
-          <p className="text-sm text-gray-800 dark:text-gray-200 font-medium">
-            {post.title}{' '}
-            <span className="text-gray-400 text-sm ml-1">
-              @usuario{post.userId}
-            </span>
-          </p>
-        }
-        content={
-          <p className="text-sm text-gray-600 dark:text-gray-400 mt-1">
-            {post.body}
-          </p>
-        }
-      />
+    <li className="list-none">
+      <button
+        onClick={() => router.push(`/posts/${post.id}`)}
+        className="w-full text-left cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2  transition duration-200"
+      >
+        <FeedItemLayout
+          avatarUrl={`https://i.pravatar.cc/150?u=${post.userId}`}
+          header={
+            <p className="text-sm text-gray-800 dark:text-gray-200 font-medium">
+              {post.title}{' '}
+              <span className="text-gray-400 text-sm ml-1">
+                @usuario{post.userId}
+              </span>
+            </p>
+          }
+          content={
+            <p className="text-sm text-gray-600 dark:text-gray-400 mt-1">
+              {post.body}
+            </p>
+          }
+        />
+      </button>
     </li>
   );
 };
